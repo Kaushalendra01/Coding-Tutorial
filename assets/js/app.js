a0be7228f5cf70448f420dddb5cfa5fb51c7bc97
Kaushalendra Kumar <91857508+Kaushalendra01@users.noreply.github.com>
2022-09-23 22:05:20 +0530

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -243,11 +243,16 @@ try {
 }
 
 
-
+// .html or index removal
 //<![CDATA[
 var uri = window.location.toString();
 if (uri.indexOf(".html",".html") > 0) {
   var clean_uri = uri.substring(0, uri.indexOf(".html"));
+  window.history.replaceState({}, document.title, clean_uri);
+}
+var uri = window.location.toString();
+if (uri.indexOf("index","index") > 0) {
+  var clean_uri = uri.substring(0, uri.indexOf("index"));
   window.history.replaceState({}, document.title, clean_uri);
 }
 //]]>
